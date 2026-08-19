a92f98d55a99c505e2e623d8ef84173a8c8eec0b
Aster0 <adenv3@gmail.com>
2026-08-19 14:02:59 +0800

    Completed Level-0

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Carl User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Carl.java
@@ -0,0 +1,24 @@
+public class Carl {
+
+
+    public static final String BOT_NAME = "CARL";
+    public static void main(String[] args) {
+        String separator = "____________________________________________________________";
+        System.out.println(separator);
+
+
+        String banner = "  ____    _    ____  _     \n"
+                + " / ___|  / \\  |  _ \\| |    \n"
+                + "| |     / _ \\ | |_) | |    \n"
+                + "| |___ / ___ \\|  _ <| |___ \n"
+                + " \\____/_/   \\_\\_| \\_\\____|\n";
+
+        System.out.println("Hello there!  I am " + BOT_NAME + ".");
+        System.out.println("What do you need help in?");
+
+        System.out.println(separator);
+        System.out.println("Bye bye! Hope to see you again soon!");
+        System.out.println(separator);
+
+    }
+}
