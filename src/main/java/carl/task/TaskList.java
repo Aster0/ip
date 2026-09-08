2192c82643773920f4dfea4714c8dc30d3524f76
Aster0 <adenv3@gmail.com>
2026-09-08 22:38:59 +0800

    Fix sorting to ignore case

--- src/main/java/carl/task/TaskList.java
+++ src/main/java/carl/task/TaskList.java
@@ -27,7 +27,7 @@ public class TaskList implements Comparator<Task>{
 
     @Override
     public int compare(Task t1, Task t2) {
-        return t1.name.compareTo(t2.name);
+        return t1.name.compareToIgnoreCase(t2.name);
     }
 
     /**
